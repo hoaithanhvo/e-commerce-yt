5618a5b58f4094cd004b842a05160348830f957d
hoaithanhvo <115620157+hoaithanhvo@users.noreply.github.com>
2024-01-15 01:23:55 +0700

    secondcommit

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -103,20 +103,19 @@ const updateProducts = asyncHandler(async (req, res) => {
         success: updateProduct ? true : false,
         updateProduct: updateProduct ? updateProduct : "Cannot update product"
 
-        //vohoaithanh
+        //voho
     })
 
 })
 
-// const deleteProduct = asyncHandler(async (req, res) => {
-//     const { pid } = req.params
-//     const deleteProducts = await Product.findByIdAndDelete(pid)
-//     return res.status(200).json({
-//         success: deleteProducts ? true : false,
-//         deleteProducts: deleteProducts ? deleteProducts : "Cannot update product"
-//     })
+const deleteProduct = asyncHandler(async (req, res) => {
+    const deleteProducts = await Product.findByIdAndDelete(pid)
+    return res.status(200).json({
+        success: deleteProducts ? true : false,
+        deleteProducts: deleteProducts ? deleteProducts : "Cannot update product"
+    })
 
-// })
+})
 const ratings = asyncHandler(async (req, res) => {
     const { _id } = req.user;
     const { star, comment, pid, updatedAt } = req.body;
